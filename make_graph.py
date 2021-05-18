7ded5cbfc01a3334aca3db4d89fbb76105b602d1
MaIlory <84338982+MaIlory@users.noreply.github.com>
2021-05-18 10:54:48 +0200

    Update make_graph.py

--- make_graph.py
+++ make_graph.py
@@ -1,1 +1,9 @@
-
+def make_graph(list):
+  for i in range(len(list)):
+    for j in range(len(list[i])):
+      if list[i][j] == 2:
+        entree = (i, j)
+      elif list[i][j] == 3:
+        sortie = (i, j)
+  graph = {}
+  co_actuel = entré
