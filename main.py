308f943cfa0fcb6f5b079f0dfecba542ef0af974
Styff <66780656+styffstyff@users.noreply.github.com>
2021-05-18 11:26:31 +0200

    Update main.py
    Ajout de la fonction open_maze() qui permet d'obtenir un labyrinthe sous forme de list de list depuis le nom d'un fichier

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+
+def open_maze(filename):
+    with open(filename, 'r', encoding='utf-8') as maze_file:
+        maze=[]
+        for line in maze_file:
+            line=[ n for n in line if n in "0123"  ]
+            maze.append(line)
+        return maze
