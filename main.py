2e361db00288a6dea9ed9f6953abdb0d83689e9e
Styff <66780656+styffstyff@users.noreply.github.com>
2021-05-21 09:40:49 +0200

    Update main.py
    Correction of variable name

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def open_maze(filepath):
     with open(filepath, 'r', encoding='utf-8') as maze_file:
         maze=[]
         for line in maze_file:
-            line=[ n for n in line if n in "0123"  ]
-            maze.append(line)
+            row=[ n for n in line if n in "0123"  ]
+            maze.append(row)
         return maze
