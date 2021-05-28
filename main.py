de22b6a866a476bd9f51deb218c352ff4da20bdc
MaIlory <84338982+MaIlory@users.noreply.github.com>
2021-05-28 08:31:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ Main module
 """
 
 import maze
+import make_graph
+import solve
 
-maze_example=maze.open_maze('./maze.txt')
+
+maze_example = maze.open_maze('./maze.txt')
+graph = make_graph.make_graph(maze_example)
+for pos, row in enumerate(maze_example)):
+    for value in row:
+        if value == 2:
+            entree=(pos,row.index(2))
+        if value == 3:
+            sortie = (pos, row.index(2))
+
+solution = solve.dead_end_filling(graph, entree, sortie)
+
 for e in maze_example:
     print(e)
